617dbe3bb458e73918c473970b074ac97a7b81a0
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2026-08-03 18:18:12 +0530

    feat: Enhance AnalyzeResume function to include response handling and save to AnalysisRepository

--- internal/orchestrator/resume.go
+++ internal/orchestrator/resume.go
@@ -1,17 +1,34 @@
 package orchestrator
 
 import (
+	"encoding/json"
+
 	"github.com/Aditya7880900936/ai-orchestrator/internal/cache"
 	models "github.com/Aditya7880900936/ai-orchestrator/internal/model"
+	"github.com/Aditya7880900936/ai-orchestrator/internal/repository"
 	"github.com/Aditya7880900936/ai-orchestrator/internal/workflow"
 )
 
 func AnalyzeResume(req models.ResumeAnalyzeRequest) (*models.ResumeAnalyzeResponse, error) {
+
 	cacheKey := cache.GenerateKey(req.ResumeText)
 
-	return ExecutePipeline[models.ResumeAnalyzeResponse](
+	resp, err := ExecutePipeline[models.ResumeAnalyzeResponse](
 		cacheKey,
 		req.ResumeText,
 		workflow.NewResumeWorkflow(),
 	)
+	if err != nil {
+		return nil, err
+	}
+
+	repo := repository.NewAnalysisRepository()
+
+	bytes, _ := json.Marshal(resp)
+
+	if err := repo.Save(req.ResumeText, string(bytes)); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
